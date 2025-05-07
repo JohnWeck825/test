bfac1e611bb5ae8254fa31503337945cc384683f
John Weck <109520995+JohnWeck825@users.noreply.github.com>
2025-05-07 18:22:22 +0700

    Create test-data.sql

---
+++ src/main/resources/templates/test-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (id, name, slug, parent_id) VALUES (1, 'Laptop', 'laptop', NULL);
+INSERT INTO category (id, name, slug, parent_id) VALUES (2, 'Laptop Gaming', 'laptop-gaming', 1);
+INSERT INTO category (id, name, slug, parent_id) VALUES (3, 'Laptop đồ họa', 'laptop-do-hoa', 1);
+INSERT INTO category (id, name, slug, parent_id) VALUES (4, 'Laptop văn phòng', 'laptop-van-phong', 1);
+
+INSERT INTO category (id, name, slug, parent_id) VALUES (5, 'Điện thoại', 'dien-thoai', NULL);
+INSERT INTO category (id, name, slug, parent_id) VALUES (6, 'Oppo', 'oppo', 5);
+INSERT INTO category (id, name, slug, parent_id) VALUES (7, 'Vivo', 'vivo', 5);
+INSERT INTO category (id, name, slug, parent_id) VALUES (8, 'Realme', 'realme', 5);
